1d284709a48f573821c943153fabb4f3e2a5b94e
Marcela Castro León <mcastrol@redhat.com>
2024-03-29 11:26:07 +0100

    testopenai

---
+++ testconn/testopenai.py
@@ -0,0 +1,37 @@
+import os
+from openai import OpenAI
+
+client = OpenAI()
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
+# client = OpenAI(
+#   api_key=os.environ.get("CUSTOM_ENV_NAME"),
+# )
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ],
+  max_tokens=50
+)
+
+print(completion.choices[0].message)
+
+
+# import openai
+# import time
+
+# from openai import OpenAI
+# client = OpenAI()
+
+# response = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a helpful assistant."},
+#     {"role": "user", "content": "Who won the world series in 2020?"},
+#     {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+#     {"role": "user", "content": "Where was it played?"}
+#   ],
+#   max_tokens=10
+# )
